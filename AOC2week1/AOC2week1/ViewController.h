bf795ebf7e6ddb574dca7d181748fe7ec16618d2
Janis Rowe <jae.rowe@yahoo.com>
2012-10-22 23:03:16 -0400

    first commit

---
+++ AOC2week1/AOC2week1/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  AOC2week1
+//
+//  Created by Janis Jae on 10/22/12.
+//  Copyright (c) 2012 Janis Jae. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
